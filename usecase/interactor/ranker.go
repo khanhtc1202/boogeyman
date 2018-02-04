49343e5ad85a73fea378bc50a42c7d8633877d3d
khanhub <khanh.tran@uzabase.com>
2018-02-04 14:13:18 +0900

    e2e test ver 1

--- usecase/interactor/ranker.go
+++ usecase/interactor/ranker.go
@@ -19,9 +19,9 @@ func NewRanker(pool repository.MaterialPool) *Ranker {
 	}
 }
 
-func (r *Ranker) Top(searchEngines []search_engine.SearchEngineType) (*domain.ResultItems, error) {
+func (r *Ranker) Top(searchEngines *search_engine.SearchEngineList) (*domain.ResultItems, error) {
 	topResults := domain.EmptyResultItems()
-	for _, searchEngine := range searchEngines {
+	for _, searchEngine := range *searchEngines {
 		searchResult, err := r.materialPool.GetItemsFromSearchEngine(searchEngine)
 		if err != nil {
 			return nil, errors.Wrap(err, "Error on fetch data from pool")
@@ -31,9 +31,9 @@ func (r *Ranker) Top(searchEngines []search_engine.SearchEngineType) (*domain.Re
 	return topResults, nil
 }
 
-func (r *Ranker) CrossMatch(searchEngines []search_engine.SearchEngineType) (*domain.ResultItems, error) {
+func (r *Ranker) CrossMatch(searchEngines *search_engine.SearchEngineList) (*domain.ResultItems, error) {
 	crossMatchedResults := domain.EmptyResultItems()
-	for _, searchEngine := range searchEngines {
+	for _, searchEngine := range *searchEngines {
 		searchResult, err := r.materialPool.GetItemsFromSearchEngine(searchEngine)
 		if err != nil {
 			return nil, errors.Wrap(err, "Error on fetch data from pool")
@@ -43,9 +43,9 @@ func (r *Ranker) CrossMatch(searchEngines []search_engine.SearchEngineType) (*do
 	return crossMatchedResults.DuplicateElements(), nil
 }
 
-func (r *Ranker) None(searchEngines []search_engine.SearchEngineType) (*domain.ResultItems, error) {
+func (r *Ranker) None(searchEngines *search_engine.SearchEngineList) (*domain.ResultItems, error) {
 	allResults := domain.EmptyResultItems()
-	for _, searchEngine := range searchEngines {
+	for _, searchEngine := range *searchEngines {
 		searchResult, err := r.materialPool.GetItemsFromSearchEngine(searchEngine)
 		if err != nil {
 			return nil, errors.Wrap(err, "Error on fetch data from pool")
