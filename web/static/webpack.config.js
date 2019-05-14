4642679e935097dd131d2e47e2e33fec804241e9
khanhub <khanh.tran@uzabase.com>
2019-05-15 02:23:58 +0900

    build view

--- web/static/webpack.config.js
+++ web/static/webpack.config.js
@@ -4,9 +4,19 @@ module.exports = {
 	entry: path.resolve(__dirname, 'app.jsx'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js'
+		filename: 'bundle.js'
 	},
 	resolve: {
 		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
 	}
 };
